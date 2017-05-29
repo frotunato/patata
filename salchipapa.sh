77085df1c4b349f6e65ae43d2a1aaddcac1ac1f2
potato <4an4zz+cq3ga95ikz26k@sharklasers.com>
2017-05-29 16:27:44 +0200

    fixes

--- salchipapa.sh
+++ salchipapa.sh
@@ -4,7 +4,8 @@
     rm -- "$0"
     exit 1
 fi
- 
+
+cd home
 sudo debconf-set-selections <<< "postfix postfix/mailname string salchipapa.llameante@gmail.com"
 sudo debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
 sudo apt-get -y install mailutils ssmtp
@@ -14,13 +15,13 @@ sudo bash -c 'echo "AuthUser=salchipapa.llameante@gmail.com" >> /etc/ssmtp/ssmtp
 sudo bash -c 'echo "AuthPass=tuculoesmio" >> /etc/ssmtp/ssmtp.conf'
 sudo bash -c 'echo "mailhub=smtp.gmail.com:587" >> /etc/ssmtp/ssmtp.conf'
 sudo bash -c 'echo "UseSTARTTLS=YES" >> /etc/ssmtp/ssmtp.conf'
-sudo chmod +x ./patata/patata
-sudo sed -i "20s/.*/\""$1"\"/" ./patata/config.txt
-sudo sed -i "18s/.*/\""$2"\"/" ./patata/config.txt
+sudo chmod +x /patata/patata
+sudo sed -i "20s/.*/\""$1"\"/" /patata/config.txt
+sudo sed -i "18s/.*/\""$2"\"/" /patata/config.txt
 sudo sysctl -w vm.nr_hugepages=128
 sudo bash -c 'echo "* soft memlock 262144" >> /etc/security/limits.conf'
 sudo bash -c 'echo "* hard memlock 262144" >> /etc/security/limits.conf'
-screen -dm sudo ./patata/patata ./patata/config.txt
+screen -dm sudo /patata/patata /patata/config.txt
  
 if [ $# -eq 3 ]
   then
