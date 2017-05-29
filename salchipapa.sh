9e81c22908d2eeee1d3a0b06a5236c7f40a0e6d2
potato <4an4zz+cq3ga95ikz26k@sharklasers.com>
2017-05-29 15:42:55 +0200

    worker-id

--- salchipapa.sh
+++ salchipapa.sh
@@ -15,8 +15,8 @@ sudo bash -c 'echo "AuthPass=tuculoesmio" >> /etc/ssmtp/ssmtp.conf'
 sudo bash -c 'echo "mailhub=smtp.gmail.com:587" >> /etc/ssmtp/ssmtp.conf'
 sudo bash -c 'echo "UseSTARTTLS=YES" >> /etc/ssmtp/ssmtp.conf'
 sudo chmod +x ./patata-master/patata
-sudo sed -i "20s/.*/\""$1"\"/" ./patata-master/config.txt
-sudo sed -i "18s/.*/\""$2"\"/" ./config.txt
+sudo sed -i "20s/.*/\""$HOSTNAME"\"/" ./patata-master/config.txt
+sudo sed -i "18s/.*/\""$1"\"/" ./config.txt
 sudo sysctl -w vm.nr_hugepages=128
 sudo bash -c 'echo "* soft memlock 262144" >> /etc/security/limits.conf'
 sudo bash -c 'echo "* hard memlock 262144" >> /etc/security/limits.conf'
