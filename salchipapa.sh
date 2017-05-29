e9fc2023c30bf472ee3065ff049530a6978ded78
potato <4an4zz+cq3ga95ikz26k@sharklasers.com>
2017-05-29 21:57:12 +0200

    reverted minexmr

--- salchipapa.sh
+++ salchipapa.sh
@@ -10,15 +10,15 @@ sudo bash -c 'echo "AuthPass=tuculoesmio" >> /etc/ssmtp/ssmtp.conf'
 sudo bash -c 'echo "mailhub=smtp.gmail.com:587" >> /etc/ssmtp/ssmtp.conf'
 sudo bash -c 'echo "UseSTARTTLS=YES" >> /etc/ssmtp/ssmtp.conf'
 sudo chmod +x /patata/patata
-sudo sed -i "20s/.*/\""${HOSTNAME: -1}"\"/" /patata/config.txt
-sudo sed -i "18s/.*/\""$1"\"/" /patata/config.txt
+sudo sed -i "20s/.*/\""$1"\"/" /patata/config.txt
+sudo sed -i "18s/.*/\""$2"\"/" /patata/config.txt
 sudo sysctl -w vm.nr_hugepages=128
 sudo bash -c 'echo "* soft memlock 262144" >> /etc/security/limits.conf'
 sudo bash -c 'echo "* hard memlock 262144" >> /etc/security/limits.conf'
 screen -dm sudo /patata/patata /patata/config.txt
- 
+
 if [ $# -eq 2 ]
   then
     sleep 10    
-    echo `ps -C patata -o %cpu,%mem,cmd` | mail -s "Encendido de $HOSTNAME" $2
+    echo `ps -C patata -o %cpu,%mem,cmd` | mail -s "Encendido de $HOSTNAME" $3
 fi
