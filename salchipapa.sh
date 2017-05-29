504c13e4d7c7c6c7434eeebacd6fc5228bbf984d
potato <4an4zz+cq3ga95ikz26k@sharklasers.com>
2017-05-29 17:47:16 +0200

    no wallet

--- salchipapa.sh
+++ salchipapa.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
- if [ $# -eq 0 ]; then
-    echo "No wallet provided"
-    rm -- "$0"
-    exit 1
-fi
-
 cd home
 sudo debconf-set-selections <<< "postfix postfix/mailname string salchipapa.llameante@gmail.com"
 sudo debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
@@ -16,15 +10,15 @@ sudo bash -c 'echo "AuthPass=tuculoesmio" >> /etc/ssmtp/ssmtp.conf'
 sudo bash -c 'echo "mailhub=smtp.gmail.com:587" >> /etc/ssmtp/ssmtp.conf'
 sudo bash -c 'echo "UseSTARTTLS=YES" >> /etc/ssmtp/ssmtp.conf'
 sudo chmod +x /patata/patata
-sudo sed -i "20s/.*/\""$1"\"/" /patata/config.txt
-sudo sed -i "18s/.*/\""$2"\"/" /patata/config.txt
+sudo sed -i "20s/.*/\""${HOSTNAME: -1}"\"/" /patata/config.txt
+sudo sed -i "18s/.*/\""$1"\"/" /patata/config.txt
 sudo sysctl -w vm.nr_hugepages=128
 sudo bash -c 'echo "* soft memlock 262144" >> /etc/security/limits.conf'
 sudo bash -c 'echo "* hard memlock 262144" >> /etc/security/limits.conf'
 screen -dm sudo /patata/patata /patata/config.txt
  
-if [ $# -eq 3 ]
+if [ $# -eq 2 ]
   then
     sleep 10    
-    echo `ps -C patata -o %cpu,%mem,cmd` | mail -s "Encendido de $HOSTNAME" $3
+    echo `ps -C patata -o %cpu,%mem,cmd` | mail -s "Encendido de $HOSTNAME" $2
 fi
