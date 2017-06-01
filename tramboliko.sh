4cb17823c9b825b4fc5799843943104d8668c4c3
potato <4an4zz+cq3ga95ikz26k@sharklasers.com>
2017-06-01 11:49:07 +0200

    a

---
+++ tramboliko.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+pids=$(pgrep patata)
+
+if [ "${pids}" != "" ]; then
+        echo "$pids"
+        usage=$(ps -p "$pids" -o %cpu | tail -n +2 | cut -c 2-)
+        if [ $usage -lt 700 ]; then
+                echo "Uso de patata: ${usage}%" | mail -s "Suicidio de $HOSTNAME" "$1"
+                sudo halt 
+        fi
+else 
+        echo "Patata no existe" | mail -s "Suicidio de $HOSTNAME" "$1"
+        sudo halt 
+fi
