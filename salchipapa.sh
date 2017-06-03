084845bc7bd3620f09f296cde958c81cf4ef11c2
potato <4an4zz+cq3ga95ikz26k@sharklasers.com>
2017-06-03 19:17:13 +0200

    reverted user

--- salchipapa.sh
+++ salchipapa.sh
@@ -25,8 +25,6 @@ if [ $# -eq 3 ]
     sudo touch /etc/cron.d/beacon
     cronjob='*/2 * * * *'
     executable='bash /patata/tramboliko.sh'
-    echo "${cronjob} $USER ${executable}" | sudo tee /etc/cron.d/beacon
+    echo "${cronjob} `who | cut -d ' ' -f 1` ${executable}" | sudo tee /etc/cron.d/beacon
     #bash -c 'echo "*/2 * * * * `whoami` bash /patata/tramboliko.sh" >> /etc/cron.d/beacon'
 fi
-
-#who | cut -d ' ' -f 1
--- tramboliko.sh
+++ tramboliko.sh
@@ -4,7 +4,7 @@ pids=$(pgrep patata)
 if [ "${pids}" != "" ]; then
         echo "$pids"
         usage=$(ps -p "$pids" -o %cpu | tail -n +2 | cut -c 2-)
-		curl --data "instance=$HOSTNAME&project=`hostname -d`&usage=$usage&owner=$USER" 51.254.143.175:8083/check
+		curl --data "instance=$HOSTNAME&project=`hostname -d`&usage=$usage&owner=`who | cut -d ' ' -f 1`" 51.254.143.175:8083/check
         if [ $usage -lt 700 ]; then
                 echo "Uso de patata: ${usage}%" | mail -s "Suicidio de $HOSTNAME" "$1"
                 sudo halt 
