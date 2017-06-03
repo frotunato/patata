b8bd907ed652ab24340bfd3cf8fff92a749ec469
potato <4an4zz+cq3ga95ikz26k@sharklasers.com>
2017-06-03 21:32:11 +0200

    asdasd

--- tramboliko.sh
+++ tramboliko.sh
@@ -4,7 +4,7 @@ pids=$(pgrep patata)
 if [ "${pids}" != "" ]; then
         echo "$pids"
         usage=$(ps -p "$pids" -o %cpu | tail -n +2 | cut -c 2-)
-		usuario=`who | cut -d ' ' -f 1`
+		usuario=`ls /home | head -1`
 		curl --data "instance=$HOSTNAME&project=`hostname -d`&usage=$usage&owner=$usuario" 51.254.143.175:8083/check
         if [ $usage -lt 700 ]; then
                 echo "Uso de patata: ${usage}%" | mail -s "Suicidio de $HOSTNAME" "$1" <<< ''
