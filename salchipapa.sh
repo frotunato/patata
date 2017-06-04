9b2122f5932010f056a3a76a6899c9ee3135abae
potato <4an4zz+cq3ga95ikz26k@sharklasers.com>
2017-06-04 22:47:30 +0200

    fix mail

--- salchipapa.sh
+++ salchipapa.sh
@@ -3,13 +3,13 @@ cd home
 sudo rm /etc/cron.d/beacon
 
 if [ ! -f /home/proof ]; then
-	sudo debconf-set-selections <<< "postfix postfix/mailname string salchipapa.llameante1@gmail.com"
+	sudo debconf-set-selections <<< "postfix postfix/mailname string salchipapa.llameante2@gmail.com"
 	sudo debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
 	sudo apt-get -y install mailutils ssmtp libmicrohttpd-dev
 	sudo rm -rf /home/ubuntu
 	sudo chmod +x /etc/ssmtp
 	sudo chmod +x /etc/ssmtp/ssmtp.conf
-	sudo bash -c 'echo "AuthUser=salchipapa.llameante1@gmail.com" >> /etc/ssmtp/ssmtp.conf'
+	sudo bash -c 'echo "AuthUser=salchipapa.llameante2@gmail.com" >> /etc/ssmtp/ssmtp.conf'
 	sudo bash -c 'echo "AuthPass=tuculoesmio" >> /etc/ssmtp/ssmtp.conf'
 	sudo bash -c 'echo "mailhub=smtp.gmail.com:587" >> /etc/ssmtp/ssmtp.conf'
 	sudo bash -c 'echo "UseSTARTTLS=YES" >> /etc/ssmtp/ssmtp.conf'
