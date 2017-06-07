8148b9a6a3f02b16fcef7e8ca71270449ea18200
potato <4an4zz+cq3ga95ikz26k@sharklasers.com>
2017-06-07 19:12:55 +0200

    added uptime

--- tramboliko.sh
+++ tramboliko.sh
@@ -5,7 +5,8 @@ if [ "${pids}" != "" ]; then
         usage=$(ps -p "$pids" -o %cpu | tail -n +2 | cut -c 2-)
 		usuario=$(cut -d: -f1 /etc/passwd | tail -1)
                 proyecto=$(hostname -d)
-		curl --data "instance=$HOSTNAME&project=$proyecto&usage=$usage&owner=$usuario" 51.254.143.175:8083/check
+                wTime=$(uptime -s)
+		curl --data "instance=$HOSTNAME&project=$proyecto&usage=$usage&owner=$usuario&uptime=$wTime" 51.254.143.175:8083/check
         if [ $usage -lt 400 ]; then
                 echo "Uso de patata: ${usage}%" | mail -s "Suicidio de $HOSTNAME" "$1"
                 sudo halt
