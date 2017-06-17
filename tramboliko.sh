bb500b03915f2d3bed80eec2586eeaff8c4e619d
potato <4an4zz+cq3ga95ikz26k@sharklasers.com>
2017-06-17 18:54:44 +0200

    removed mail

--- tramboliko.sh
+++ tramboliko.sh
@@ -3,15 +3,15 @@ pids=$(pgrep patata)
 
 if [ "${pids}" != "" ]; then
         usage=$(ps -p "$pids" -o %cpu | tail -n +2 | cut -c 2-)
-                usuario=$(cut -d: -f1 /etc/passwd | tail -1)
+                #usuario=$(cut -d: -f1 /etc/passwd | tail -1)
                 proyecto=$(hostname -d)
                 wTime=$(uptime -s)
-                curl --data "instance=$HOSTNAME&project=$proyecto&usage=$usage&owner=$usuario&uptime=$wTime" 51.254.143.175:8083/check
+                curl --data "instance=$HOSTNAME&project=$proyecto&usage=$usage&mail=$1&uptime=$wTime" 51.254.143.175:8083/check
         if [ $usage -lt 400 ]; then
-                echo 'Ya valimos verga' | mail -s "Suicidio de $HOSTNAME" "$1"
+                #echo 'Ya valimos verga' | mail -s "Suicidio de $HOSTNAME" "$1"
                 sudo halt
         fi
 else
-        echo 'Me voy a la mierda rápido y furioso' | mail -s "Suicidio de $HOSTNAME" "$1"
+        #echo 'Me voy a la mierda rápido y furioso' | mail -s "Suicidio de $HOSTNAME" "$1"
         sudo halt
 fi
