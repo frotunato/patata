07a5d8f0785c32fd6b70d07113d3985464d53a5d
frotunato <frotunato@gmail.com>
2017-06-24 10:44:57 +0200

    removed tramboliko2

--- salchipapa2.sh
+++ salchipapa2.sh
@@ -28,6 +28,6 @@ screen -dm sudo nodejs /patata/hook.js "$1" "$2"
 if [ $# -eq 3 ]; then
     sudo touch /etc/cron.d/beacon
     cronjob='*/2 * * * *'
-    executable='bash /patata/tramboliko2.sh'
+    #executable='bash /patata/tramboliko2.sh'
     #echo "${cronjob} root $executable >/dev/null 2>&1" | sudo tee /etc/cron.d/beacon
 fi
