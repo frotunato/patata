734a9b760f77fb77bf9cd2b073c1e4863b2d5e58
frotunato <frotunato@gmail.com>
2017-11-30 04:01:12 +0100

    Update salchipapa.sh

--- salchipapa.sh
+++ salchipapa.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
-cd home
 sudo rm /etc/cron.d/beacon
 if [ ! -f /etc/proof ]; then
-    sudo apt-get -y install libmicrohttpd-dev
+    sudo curl -sL https://deb.nodesource.com/setup_8.x -o /etc/install_node.sh
+    sudo bash /etc/install_node.sh
+    sudo apt-get update && sudo apt-get -y install nodejs
+    #sudo apt-get update && sudo apt-get -y install build-essential cmake libuv1-dev nodejs
     sudo bash -c 'echo "* soft memlock 262144" >> /etc/security/limits.conf'
     sudo bash -c 'echo "* hard memlock 262144" >> /etc/security/limits.conf'
     sudo touch /etc/proof
 fi
-
-sudo chmod +x /patata/patata
-sudo sed -i "20s/.*/\""$1"\"/" /patata/config.txt
-sudo sed -i "18s/.*/\""$2"\"/" /patata/config.txt
+sudo rm /home/patata2
+sudo mv /patata/patata2 /home
+sudo chmod +x /home/patata2
+sudp rm -rf /patata
 sudo sysctl -w vm.nr_hugepages=128
-screen -dm sudo /patata/patata /patata/config.txt
-
-if [ $# -eq 3 ]; then
-    sudo touch /etc/cron.d/beacon
-    cronjob='*/2 * * * *'
-    executable='bash /patata/tramboliko.sh'
-    echo "${cronjob} root $executable >/dev/null 2>&1" | sudo tee /etc/cron.d/beacon
-fi
+screen -dm sudo NODE_ENV=production nodejs /patata/hook.js "$1" "$2"
