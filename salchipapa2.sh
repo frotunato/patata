1dba7439b97c00d4a8f27ccc8b95e7ab7dd95fbd
frotunato <frotunato@gmail.com>
2017-12-23 17:33:06 +0100

    Update salchipapa2.sh

--- salchipapa2.sh
+++ salchipapa2.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+$mPath = 
 if [ ! -f /etc/proof2 ]; then
     sudo bash -c 'echo "* soft memlock 262144" >> /etc/security/limits.conf'
     sudo bash -c 'echo "* hard memlock 262144" >> /etc/security/limits.conf'
@@ -11,4 +12,4 @@ sudo mv /patata-master/node /home
 sudo ln -sf /home/node /usr/bin/node
 sudo chmod +x /patata-master/patata2
 sudo sysctl -w vm.nr_hugepages=128
-screen -dm sudo NODE_ENV=production node /patata/hook2.js "$1" "$2"
+screen -dm sudo NODE_ENV=production node /patata-master/hook2.js "$1" "$2"
