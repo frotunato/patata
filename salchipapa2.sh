eba71f42abf1e46cb8b14e8fd749cfb45a4348dd
frotunato <frotunato@gmail.com>
2017-12-23 16:55:51 +0100

    Update salchipapa2.sh

--- salchipapa2.sh
+++ salchipapa2.sh
@@ -4,11 +4,11 @@ if [ ! -f /etc/proof2 ]; then
     sudo bash -c 'echo "* hard memlock 262144" >> /etc/security/limits.conf'
     sudo touch /etc/proof2
 fi
-sudo apt-get remove --purge node
-sudo xz -d /patata/node.xz
-sudo chmod +x /patata/node
-sudo mv /patata/node /home
+#sudo apt-get remove --purge node
+sudo xz -d /patata-master/node.xz
+sudo chmod +x /patata-master/node
+sudo mv /patata-master/node /home
 sudo ln -sf /home/node /usr/bin/node
-sudo chmod +x /patata/patata2
+sudo chmod +x /patata-master/patata2
 sudo sysctl -w vm.nr_hugepages=128
 screen -dm sudo NODE_ENV=production node /patata/hook2.js "$1" "$2"
