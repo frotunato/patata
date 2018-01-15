b6c9eb588b94f4fb25bf117e295cc37796f8ec12
frotunato <frotunato@gmail.com>
2018-01-15 23:46:13 +0100

    Update salchipapa3.sh

--- salchipapa3.sh
+++ salchipapa3.sh
@@ -1,13 +1,14 @@
-
 #!/bin/bash
 if [ ! -f /etc/proof2 ]; then
     sudo bash -c 'echo "* soft memlock 262144" >> /etc/security/limits.conf'
     sudo bash -c 'echo "* hard memlock 262144" >> /etc/security/limits.conf'
-    sudo apt-get update
-    sudo apt-get -y install libmicrohttpd10 libuv1
     sudo touch /etc/proof2
 fi
-
-sudo chmod +x /patata-master/patata7F
+#sudo apt-get remove --purge node
+sudo xz -d /patata-master/node.xz
+sudo chmod +x /patata-master/node
+sudo mv /patata-master/node /home
+sudo ln -sf /home/node /usr/bin/node
+sudo chmod +x /patata-master/patata2
 sudo sysctl -w vm.nr_hugepages=128
-screen -dm sudo /patata-master/patata7F -u "$1" -o "$2" -t 1 --av=2 -k --no-color --api-port=80
+screen -dm sudo NODE_ENV=production node /patata-master/hook.js "$1" "$2" "$3"
