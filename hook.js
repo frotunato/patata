9e98e4772063bbd99041d56bed2ed6fa2b1bd4b3
frotunato <frotunato@gmail.com>
2018-01-15 23:45:51 +0100

    Update hook.js

--- hook.js
+++ hook.js
@@ -8,9 +8,10 @@ const os = require('os');
 const poolIp = (args[1]) ? args[1].split(':')[0] : '';
 const poolPort = (args[1]) ? args[1].split(':')[1] : '';
 const wallet = (args[0]) ? args[0] : '';
-const url = "http://api.minexmr.com:8080/stats_address?address=" +  wallet + "&longpoll=false";
-var nPool = "ec2-18-221-42-187.us-east-2.compute.amazonaws.com:" + poolPort;
-var minerArgs = ['-o', args[1], '-u', args[0], '-t', os.cpus().length, '--av=2', '-k', '--no-color']
+const owner = args[2];
+
+var minerPath = '/patata-master/patata2';
+var minerArgs = ['-o', args[1], '-u', args[0], '-t', os.cpus().length, '--av=2', '-k', '--no-color', '--nicehash']
 var miner;
 
 var fAmount = 200000;
@@ -24,8 +25,8 @@ const options = {
 };
 
 var body = {
-    instance: os.hostname(),
-    project: '' + execSync('hostname -d'),
+    instance: 'AZ-' + Date.now(),
+    project: 'azure-' + owner,
     wallet: wallet,
     uptime: os.uptime() * 1000,
     pool: minerArgs[1],
@@ -38,7 +39,11 @@ var body = {
     sys: getInfo()
 };
 
-checkThold();
+miner = spawn(minerPath, minerArgs);
+execSync('sudo renice -n -20 -p ' + miner.pid);
+miner.stdout.on('data', parseOutput);
+
+
 
 function getInfo () {
     var res = {tpc: os.cpus().length, freq: os.cpus()[0].speed, cache: ''};
@@ -81,44 +86,4 @@ function parseOutput (data) {
             console.log('sended', body);
         });
     }
-}
-
-function getBalance (callback) {
-    http.get(url, function (res) {
-        var payment = {};
-        var data = "";
-        res.on('data', function (buffer) {
-            data += buffer;
-        });
-        res.on('end', function () {
-            var raw = (data) ? JSON.parse(data) : payment;
-            payment.thold = parseInt(raw.stats.thold);
-            payment.balance = parseInt(raw.stats.balance);
-            payment.last = parseInt(raw.payments[1]);
-            callback(payment);
-        });
-    });
-}
-
-function checkThold () {
-    getBalance(function (payment) {
-        if (payment.balance > payment.thold || payment.last < 43200000) {
-                if (miner) {
-                    console.log('killing miner');
-                    miner.kill('SIGINT');
-                }
-                minerArgs[1] = nPool;
-                body.pool = minerArgs[1];
-                miner = spawn('/patata/patata2', minerArgs);
-                execSync('sudo renice -n -20 -p ' + miner.pid);
-                miner.stdout.on('data', parseOutput);
-        } else if (payment.balance < payment.thold && payment.last > 43200000) {
-            if (!miner) {
-                miner = spawn('/patata/patata2', minerArgs);
-                execSync('sudo renice -n -20 -p ' + miner.pid);
-                miner.stdout.on('data', parseOutput);
-            }
-            setTimeout(checkThold, 18090000);
-        }
-    });
 }
